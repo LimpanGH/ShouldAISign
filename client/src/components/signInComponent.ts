922f224a0d94215008152edc1c2495590a8153d8
LimpanGH <Linusj555@gmail.com>
2024-10-11 15:39:34 +0200

    user now receiving jwt in FE upon login

--- client/src/components/signInComponent.ts
+++ client/src/components/signInComponent.ts
@@ -4,8 +4,11 @@ import { SignInFormData } from '../pages/SignIn';
 const SIGN_IN_MUTATION = `
   mutation login ($email: String!, $password: String!) {
     login(email: $email, password: $password) {
+    token 
+    user {
       id
       email
+    }
     }
   }
 `;
@@ -16,14 +19,18 @@ const SignInComponent = {
       email: data.email,
       password: data.password,
     };
-    
+
     try {
       const result = await makeGraphQLRequest(SIGN_IN_MUTATION, variables);
       if (result.errors) {
         console.error(result.errors[0].message);
         alert(`Error:${result.errors[0].message}`);
       } else {
+        const {token} = result.data.login
         console.log('User logged in:', result.data.login);
+        alert('User successfully logged in!');
+
+        localStorage.setItem('token', token);
         alert('User successfully logged in!');
       }
     } catch (error) {
--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,8 +1,11 @@
 export const makeGraphQLRequest = async <T>(query: string, variables: T) => {
+  const token = localStorage.getItem('token');
+
   const response = await fetch('http://localhost:4000/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      ...(token && { Authorization: `Bearer ${token}` }),
     },
     body: JSON.stringify({ query, variables }),
   });
